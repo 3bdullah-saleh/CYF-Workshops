cf69c5f4afa3812c64281aaeaffb35fa7edd42de
Nahom Mesfin <mnahom545@gmail.com>
2025-07-19 12:50:46 +0100

    header and main fixed

--- dom-merge-conflict/tasks/buttons-and-counter/src/main.js
+++ dom-merge-conflict/tasks/buttons-and-counter/src/main.js
@@ -1,5 +1,8 @@
-  export const main = document.createElement("main");
+  export function createMain() {
+  const main = document.createElement("main");
   main.innerHTML = `
         <p id="counter" data-testid="counter">0</p>
         <button id="increment">Increment</button>
     `;
+    return main;
+  }
